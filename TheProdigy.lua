bdcd165c72721e82ab0d2b464c118fd58b486418
Isa1997 <stefan.i@hotmail.rs>
2021-07-06 03:03:56 -0700

    Test for sound

--- TheProdigy.lua
+++ TheProdigy.lua
@@ -1,27 +1,8 @@
-local MSQ = LibStub("Masque", true)
-if not MSQ then return end
+
 
 local AddOn, Core = ...
 
 local Version = GetAddOnMetadata(AddOn, "Version")
 local Websites = { "https://github.com/Isa1997/TheProdigy" }
 
-MSQ:AddSkin("TheProdigy", {
-	--Info
-	Author = "Isa1997",
-	Version = Version,
-	Description = "The Prodigy Theme",
-	Shape = "Square",
-	API_VERSION = 90002,
-	Websites = Websites,
-	Normal = {
-		Width = 40,
-		Height = 40,
-		Color = {0, 0, 0, 1},
-		Point = "CENTER",
-		RelPoint = "CENTER",
-		OffsetX = 0,
-		OffsetY = 0,
-		Texture = [[Interface\AddOns\Masque_Umbra\Textures\ant]],
-	}
-}, true)
+PlayMusic(642878)
